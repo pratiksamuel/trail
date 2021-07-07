1e2b0507ca9474060f53257cf820ea25bac9cea4
pratiksamuel <patrickabhraham12345@gmail.com>
2021-07-07 16:48:04 +0530

    ping file modified

--- ping_ip.py
+++ ping_ip.py
@@ -5,7 +5,7 @@ for ping in range(10,22):
     address = "192.168.0." + str(ping)
     res = subprocess.call(['ping', '-c', '3', address])
     if res == 0:
-        print ("ping to", address, "OK")
+        print ("ping to this ip", address, "OK")
     elif res == 2:
         print ("no response from", address)
     else:
