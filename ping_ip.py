c5a6f31423f0c032d6da32ea6e42ca7c2d46824f
pratiksamuel <patrickabhraham12345@gmail.com>
2021-07-07 16:42:23 +0530

    ping file added

---
+++ ping_ip.py
@@ -0,0 +1,12 @@
+# typo error in import
+import subprocess
+
+for ping in range(10,22):
+    address = "192.168.0." + str(ping)
+    res = subprocess.call(['ping', '-c', '3', address])
+    if res == 0:
+        print ("ping to", address, "OK")
+    elif res == 2:
+        print ("no response from", address)
+    else:
+        print ("ping to", address, "failed!")
