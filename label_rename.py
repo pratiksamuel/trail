203b8bbbcecc3aed4dfcb085352a3dd2d773f20f
pratiksamuel <patrickabhraham12345@gmail.com>
2021-07-07 16:38:20 +0530

    first commit

---
+++ label_rename.py
@@ -0,0 +1,19 @@
+import json
+import glob
+import os
+path="/home/rd/Documents/dust7/*"
+for i in glob.glob(path):
+	for j in glob.glob(i+"/*.json"):
+		print(j)
+		with open(j,"r+")as f:
+			js=json.load(f)
+			print(js['imagePath'])
+			new_name=os.path.basename(j).split(".j")[0]
+			for k in range(len(js["shapes"])):
+				if js["shapes"][k]["label"] =="others":
+					js["shapes"][k]["label"]="other"
+					flag=True
+		if flag=True:
+			with open(j,"w+") as f:
+				json.dump(js,f)
+				flag=False
