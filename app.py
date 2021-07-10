1a0f6f1b1160fe777902de8f1d2b2b4d72c833ef
pratiksamuel <patrickabhraham12345@gmail.com>
2021-07-10 11:13:40 +0530

    files added

--- app.py
+++ app.py
@@ -4,12 +4,23 @@ import random
 app = Flask(__name__)
 
 var=["aasdas","bqweqwe","ddfgdfgd"]
+a=2.18
 
 @app.route('/')
 def Home():
     single = random.choice(var)
-    
+    print("yo")
     return single
+
+@app.route('/new_page/<name>')
+def new_page(name):
+    new = random.choice(var)
+    return new+"_new_page_"+name+str(a)
+
+@app.route('/yo')
+def yo():
+    new = random.choice(var)
+    return new+"_yo_"+str(a)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port =int(os.environ.get("PORT",4000)))
